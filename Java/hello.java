7cabf2efe0fd2ed3c35a7e13e05a173b95263b19
刘凌凌 <1044238453@qq.com>
2024-07-19 17:16:07 +0800

    600

--- Java/hello.java
+++ Java/hello.java
@@ -1,7 +1,57 @@
-
+import java.util.Scanner;
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("666");
 		
+		// Scanner myScanner = new Scanner(System.in);
+		// System.out.println("请输入名字");
+
+		// String name = myScanner.next();
+		// System.out.println("请输入年龄");
+
+		// int age = myScanner.nextInt();
+		// System.out.println("请输入薪水");
+
+		// double sal = myScanner.nextDouble();
+		// System.out.println("人的信息如下:");
+		// System.out.println("名字=" + name 
+		// + " 年龄=" + age + " 薪水=" + sal );
+
+		// int b = -1>>2;
+		// System.out.println("b="+b); //-1
+		// int d = -1<<2;
+		// System.out.println("d="+d); // -4
+  /**
+   1.先打印一个5*5矩形
+	   *****
+		 *****
+		 *****
+		 *****
+		 *****
+	 2.打印半个金字塔
+	   *     // 第1层  有1个*
+		 **    // 第2层  有2个*
+		 ***   // 第3层  有3个*
+		 ****  // 第4层  有4个*
+		 ***** // 第5层  有5个*
+
+	 3.打印整个金字塔
+	       *     // 第1层  有1个*  2*1-1  有4个空格
+		    ***    // 第2层  有3个*  2*2-1  有3个空格
+		   *****   // 第3层  有5个*  2*3-1  有2个空格
+		  *******  // 第4层  有7个*  2*4-1  有1个空格
+		 ********* // 第5层  有9个*  2*5-1  有0个空格
+
+	 4.打印空心金字塔
+	       *     // 第1层  首位和末位个*
+		    * *    // 第2层  首位和末位个*
+		   *   *   // 第3层  首位和末位个*
+		  *     *  // 第4层  首位和末位个*
+		 ********* // 第5层  全部有*
+
+  */
+		
+		
+
 	}
+
 }
